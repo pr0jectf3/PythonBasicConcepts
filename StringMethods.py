c52acdb3ef62a617ca528efb4f1f24c75e71b549
pr0jectf3 <portillozachary@gmail.com>
2018-06-04 01:18:20 -0700

    a few methods added from the string class that are improtant to any pthon project

---
+++ StringMethods.py
@@ -0,0 +1,29 @@
+"string".format("name")
+
+#method that counts the amount of 'a' in a string
+"Happy birthday".count("a")
+
+#method that changes the upper and lower case
+x = "Hello My Name Is Zach"
+x.lower()
+print(x)
+x.upper()
+print(x)
+
+#method that capatalizes the first letter in the string
+print(x.capitalize())
+
+#methods that return a boolean
+print(x.islower())
+print(x.isupper())
+print(x.isalpha())
+print("absdf".isalpha())
+print("120494".isdigit())
+
+#Strings are case sensitive
+print(x.find("I"))
+
+#String count blank spaces
+name = input
+name = input("what is your name?")
+
