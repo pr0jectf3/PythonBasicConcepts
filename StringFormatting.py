c7f5535aeccfe99de3a465e77f54d25ce67dd6b1
pr0jectf3 <portillozachary@gmail.com>
2018-06-04 02:11:35 -0700

    added an important concept for string formatting in python

---
+++ StringFormatting.py
@@ -0,0 +1,8 @@
+#This is how you get user input (IMPORTANT FORMATTING CONCEPT)
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Your name is {} and age {}".format(name,age))
+
+#OR
+
+print("Your name is {0} and age {1}".format(name, age))
