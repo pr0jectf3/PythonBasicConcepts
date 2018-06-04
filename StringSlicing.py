1265f084bfe83dab2269b9b88a4ae6cdbafb4ec7
pr0jectf3 <portillozachary@gmail.com>
2018-06-04 02:27:54 -0700

    String slicing by splitting up the username and domain from a user given email

--- StringSlicing.py
+++ StringSlicing.py
@@ -6,3 +6,11 @@ print(info[8:14:])
 #how to reverse a string
 x = "hello"
 print(x[::-1])
+
+#slice the name from the input email
+email = input("input your email")
+username = email[:email.index("@"):]
+domain = email[email.index("@") + 1: email.index("."):]
+
+#print user information
+print("Your username is {} and your domain is {}".format(username, domain))
