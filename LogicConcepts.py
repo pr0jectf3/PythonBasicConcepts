c9f678a20fd509e782a0d378244cc52ff0cd0ce5
pr0jectf3 <portillozachary@gmail.com>
2018-06-04 02:44:05 -0700

    understanded the use of if-else statements in python(not, elif, or, and)

---
+++ LogicConcepts.py
@@ -0,0 +1,19 @@
+#learning if statements in python
+
+
+x = 1
+y = 2
+
+if x > y:
+    print("x is a larger number")
+else:
+    print("y is a larger number")
+
+
+weather = "Sunny"
+if(weather == "Sunny"):
+    print("wear something light because it is sunny!")
+elif(weather == "Winter"):
+    print("wear something warm")
+else:
+    print("unknown")
